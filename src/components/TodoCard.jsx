cb2661275d9d8a65ca2bd40453656692029cc3a5
nanamingg <nana172@kakao.com>
2023-11-14 17:23:00 +0900

    up

---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const TodoCard = ({ todo }) => {
+  useEffect(() => console.log(todo), []);
+
+  return (
+    <li
+      className={`h-12 flex items-center text-xl ${
+        todo.isDone && "line-through"
+      }`}
+    >
+      <span className="w-1/12 text-right">{todo.id}</span>
+      <span className="w-8/12 pl-2">{todo.title}</span>
+      <Link
+        to={`/${todo.id}?title=${todo.title}&is-done=${todo.isDone}`}
+        className="w-3/12 hover:font-bold"
+      >
+        Detail
+      </Link>
+    </li>
+  );
+};
+
+export default TodoCard;
